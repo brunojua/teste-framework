870273c5ac048d3afca5275cfe75b798c4bb114a
Bruno Sampaio <brunog1jua@gmail.com>
2021-05-18 19:13:39 -0300

    Algular meterial, bootstrap, toolbar

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+		MatButtonModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
